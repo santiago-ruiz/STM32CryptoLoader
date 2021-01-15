f51b4ea81c59e7cfe7e4fcde107d7f5dcf409669
Santiago Ruiz <santiago.ruiz.ee@gmail.com>
2021-01-15 14:15:17 -0300

    Jump to app function added

--- BOOTLOADER/Core/Inc/boot.h
+++ BOOTLOADER/Core/Inc/boot.h
@@ -5,7 +5,7 @@
 #include "ff.h"
 #include "image.h"
 #include "build_config.h"
-
+#include "flash_layer.h"
 
 void boot_fsm(void);
 
--- BOOTLOADER/Core/Src/boot.c
+++ BOOTLOADER/Core/Src/boot.c
@@ -10,6 +10,8 @@ typedef enum{
 }boot_state;
 
 static bool boot_check_condition(void);
+static void jump_to_app(void);
+
 
 static boot_state state = INIT;
 FIL bin_file;
@@ -53,6 +55,8 @@ void boot_fsm(void){
 			break;
 
 		case JUMPING:
+			f_close(&bin_file);
+			jump_to_app();
 			break;
 
 		case FAIL:
@@ -67,4 +71,21 @@ static bool boot_check_condition(void){
 	//This function check for specific condition
 	//e.g. a pin status or a button pressed for 2 seconds
 	return true;
+}
+
+pFunction Jump_To_Application;
+uint32_t JumpAddress;
+
+static void jump_to_app(void){
+
+	if (((*(__IO uint32_t*)APPLICATIONADDRESS) & 0x2FFE0000 ) == 0x20000000)
+	{
+	  /* Jump to user application */
+	  JumpAddress = *(__IO uint32_t*) (APPLICATIONADDRESS + 4);
+	  Jump_To_Application = (pFunction) JumpAddress;
+	  /* Initialize user application's Stack Pointer */
+	  __set_MSP(*(__IO uint32_t*) APPLICATIONADDRESS);
+	  Jump_To_Application();
+	}
+
 }
